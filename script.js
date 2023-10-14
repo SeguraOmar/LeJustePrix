a2c7c31f312076e6df99c97974e88301fd2b2471
Omar <omarsegura76300@gmail.com>
2023-10-14 14:21:39 +0200

    modif

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-document.write("<body>")
-let array = [{name : "Manette PS5", img : "Image1.jpg"}]
-let n = parseInt( Math.random() * array.length)
-while (isNaN(n)) {
-  n = parseInt( Math.random() * array.length)
+let array  = [
+    ['Manette PS5', 'Image1.jpg'],
+    ['Watercooling Frozen Notte', 'Image2.jpg'],
+    ['SSD NVME 1TO', 'Image3.jpg'],
+    ["Ventilateur TL C-12 C-S", "Image4.jpg"],
+    ["Boitier NZXT H5 FLOW", "Image5.jpg"]
+
+  ]
+
+document.getElementById("#tableau").innerHTML = array
+
+  function photo() {
+    let photo = document.querySelector('#tableau')
+    photo.innerHTML=`<img src="image/Image${Math.floor((Math.random()* 5) + 1)}.jpg" alt="image article" >`
 }
 
-document.write("<img src='image/Image" + array[n] + "' alt='Image aléatoire'>");
+photo()
